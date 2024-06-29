6b6890e369e79d01ae27c84428beea1471c427cf
kashish567 <kashishbhanushali7@gmail.com>
2024-06-29 14:36:33 +0530

    feat: clerk initialized and middleware for clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import Image from "next/image";
+import {Button} from "@/components/ui/button"
 
 export default function Home() {
   return (
    <div>
+    <h2>
+      Welcome!
+      </h2>
+      <Button > Hello</Button>
     </div>
   );
 }
