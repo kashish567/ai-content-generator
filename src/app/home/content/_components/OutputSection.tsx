03f2b8f17bbced455710c2234fac2377b0ef3cc7
kashish567 <kashishbhanushali7@gmail.com>
2024-06-30 22:30:39 +0530

    feat: ui -text editor done

--- src/app/home/content/_components/OutputSection.tsx
+++ src/app/home/content/_components/OutputSection.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useRef } from "react";
+import "@toast-ui/editor/dist/toastui-editor.css";
+import { Editor } from "@toast-ui/react-editor";
+import { Button } from "@/components/ui/button";
+import { Copy } from "lucide-react";
 
 const OutputSection = () => {
-  return <div>OutputSection</div>;
+  const editorRef: any = useRef();
+  return (
+    <div className="bg-white shadow-lg border rounded-lg">
+      <div className="flex justify-between items-center p-4">
+        <h2 className="font-medium text-lg">Your Result</h2>
+        <Button className="flex gap-2">
+          <Copy className="w-4 h-4" />
+          Copy
+        </Button>
+      </div>
+      <Editor
+        ref={editorRef}
+        initialValue="Your Result will appear here!"
+        initialEditType="wysiwyg"
+        previewStyle="vertical"
+        height="400px"
+        useCommandShortcut={true}
+        onChange={() =>
+          console.log(editorRef.current.getInstance().getMarkdown())
+        }
+      />
+    </div>
+  );
 };
 
 export default OutputSection;
